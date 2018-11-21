82aa88db7be4bd3152ed9b335e6f16aa95802628
Yann Barraud <yann.barraud@gmail.com>
2018-11-21 22:25:45 +0100

    Config

--- datagen.js
+++ datagen.js
@@ -25,7 +25,7 @@ function generateAccount() {
         accountIban: faker.finance.iban(true),
         accountBic: faker.finance.bic(),
         accountBalance: parseFloat(faker.finance.amount(0, 250000)),
-        transactions : generateTransactions(15)
+        transaction : generateTransactions(15)
     }
 
     return account;
@@ -49,9 +49,9 @@ function generatePerson() {
             street: faker.address.streetAddress(),
             city: faker.address.city(),
             zipCode: faker.address.zipCode(),
-            coordinates: [faker.address.latitude(), faker.address.longitude()]
+            coordinates: faker.address.latitude() + ", " + faker.address.longitude()
         },
-        accounts:generateAccounts(3)
+        account: generateAccounts(3)
     }
     return person;
 }
@@ -59,8 +59,8 @@ function generatePerson() {
 fs.unlink("./data.json", function(err){
 });
 
-for (i = 0 ; i < 50000; i++){
+for (i = 0 ; i < 500000; i++){
     var p = generatePerson();
     //console.log(util.inspect(JSON.stringify(p), {showHidden: false, depth:null}));
-    fs.appendFileSync("./data.json", JSON.stringify(p) + "\n");
+    fs.appendFileSync("./data.json", JSON.stringify(p) + "\r\n");
 }
